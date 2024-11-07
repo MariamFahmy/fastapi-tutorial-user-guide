56d67755f19faa0f0f60c178a105ec022f84decb
MariamFahmy <mariamff11@gmail.com>
2024-11-07 14:11:11 -0330

    Query Parameters and String Validations

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str | None = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items

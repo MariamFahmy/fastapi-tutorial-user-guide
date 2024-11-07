059cd204238267cf16c00e29d9af1dcff329ab6d
MariamFahmy <mariamff11@gmail.com>
2024-11-07 10:07:18 -0330

    Path Parameters

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from enum import Enum
+
 from fastapi import FastAPI
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
-    resnet = "resenet"
+    resnet = "resnet"
     lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -13,7 +16,12 @@ app = FastAPI()
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
+
+    if model_name.value == ModelName.lenet.value:
         return {"model_name": model_name, "message": "LeCNN all the images"}
-    
+
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
